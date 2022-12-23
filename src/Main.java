5ec6f2773a8ee19e15b09b0a9e0dd49e6b2ad6e6
ahkubek <120653318+ahkubek@users.noreply.github.com>
2022-12-23 08:05:39 +0300

    задача 2

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,14 @@ public class Main {
     var cat = 3.6;
     var paper = 763789;
         System.out.println(dog);
+        dog=dog+4;
+        System.out.println(dog);
+        System.out.println(cat);
+        cat=cat+4;
         System.out.println(cat);
         System.out.println(paper);
+        paper=paper+4;
+        System.out.println();
 
 
     }
